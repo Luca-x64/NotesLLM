94a9995a40e62dc58dfef89c00ea45ee14513d96
Luca <luca.ghir04@gmail.com>
2026-09-03 12:54:03 +0200

    Deployed with Docker

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import os
 from fastapi import FastAPI, HTTPException, Response, status
-
+from pydantic import BaseModel
 from note import Note
 
 OLLAMA_MODEL = os.environ.get("OLLAMA_MODEL", "llama3.2:1b")
 OLLAMA_BASE_URL = os.environ.get("OLLAMA_BASE_URL", "") # TODO
-DBPATH = os.environ.get("DBPATH", "") # TODO
 TIMEOUT_REQUEST = int(os.environ.get("TIMEOUT_REQUEST", 2)) 
 
 
 
 app = FastAPI()
 
-
-from pydantic import BaseModel
 
 class NoteUpdateModel(BaseModel):
     title: str | None
