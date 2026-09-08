2dcbf748e95fe7d192ec6dac835237ded47d5878
Luca <luca.ghir04@gmail.com>
2026-09-08 12:28:19 +0200

    data validation

--- README.md
+++ README.md
@@ -1,26 +1,13 @@
 # NotesLLM
 
 
-### TODO
-- search api validation date consistency
-- pydantic validation
-    - il titolo è obbligatorio;
-    - il titolo non può essere vuoto;
-    - il titolo non può essere composto solamente da spazi;
-    - il corpo non può superare i 3.000 caratteri;
-    - la data non può essere fornita o modificata dall’utente;
-    - non è possibile leggere, modificare, eliminare o elaborare tramite AI una nota inesistente
-
-- 422 Unprocessable Entity per dati non validi;
-
-
----
-
-
-Run with docker: 
+Data persistence is handled using an SQLite database, chosen to simplify CRUD operations, especially filtering.
 
 - `sudo docker compose up -d ollama`
-- `sudo docker compose exec ollama ollama pull llama3.2:3b` (or other model) 
+- `sudo docker compose exec ollama ollama pull [model]` 
 - `sudo docker compose up --build`
 
-Modello utilizzato: `llama3.2:3b` causa mancanza di GPU (CPU i5 12500T / RAM 32GB @3000Mhz) è un buon compromesso tra capacità e prestazioni.
+for example : `sudo docker compose exec ollama ollama pull llama3.2:3b`
+
+The model i used is: `llama3.2:3b` due to lack of a dedicated GPU.
+On an Intel core i5-12500T with 32GB or RAM it provides a good balance between inference performance and response quality. 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import FastAPI,status
 from contextlib import asynccontextmanager
-from model import NoteSearchModel, NoteUpdateModel
+from model import NoteCreateModel, NoteSearchModel, NoteUpdateModel
 from connection import connect_to_db as conn 
 from connection import create_db
 from utility import *
@@ -14,12 +14,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-
 @app.get("/notes/{note_id}",status_code=status.HTTP_200_OK)  
 def get_note(note_id: int):
     with conn() as con:
         cur = con.cursor()
-        query = "SELECT * FROM notes WHERE id = ?"
+        query = "SELECT id, title, date FROM notes WHERE id = ?"
         cur.execute(query, (note_id,))
         note = cur.fetchone()
 
@@ -38,10 +37,6 @@ def get_note(limit: int = 20):
 
 @app.post("/notes/edit/{note_id}",status_code=status.HTTP_200_OK)
 async def edit_note(note_id:int, note_update: NoteUpdateModel):
-    if (note_update.title is None) and (note_update.body is None): # CHECK maybe not needed for pydantic validation
-            raise unprocessableEntityException()
-
-    
     with conn() as con:
         cur = con.cursor()
         query = """UPDATE NOTES SET 
@@ -59,9 +54,7 @@ async def edit_note(note_id:int, note_update: NoteUpdateModel):
     
 
 @app.post("/notes/create",status_code=status.HTTP_201_CREATED)
-async def create_note(note: NoteUpdateModel):
-    if (note.title is None) and (note.body is None):
-            raise unprocessableEntityException()
+async def create_note(note: NoteCreateModel):
     with conn() as con:
         cur = con.cursor()
 
@@ -82,17 +75,12 @@ async def delete_note(note_id: int):
         raise notfoundException(note_id)
 
 
-
-
 @app.post("/notes/search/",status_code=status.HTTP_200_OK) 
 def search_notes(searchparams: NoteSearchModel):
     title = searchparams.title
     date = searchparams.date
     fd = searchparams.first_date
     sd = searchparams.second_date
-
-    if (title is None) and (date is None) and (fd is None) and (sd is None): # TODO check if fd is present and sd is not present or vice versa, then raise exception, then check if fd is greater than sd, then raise exception 
-        raise unprocessableEntityException()
     
     with conn() as con:
         cur = con.cursor()
@@ -131,11 +119,13 @@ def get_notebody_by_id(note_id: int):
 @app.get("/notes/summarize_body/{note_id}",status_code=status.HTTP_200_OK) 
 async def summarize_body(note_id: int):
     body = get_notebody_by_id(note_id)
-    response = ask_model("Sei un assistente che riassume testi in italiano, generando riassunti brevi e coerenti, basati solo sul contenuto del testo, non aggiungere parole che non fanno riferimento al riassunto. Il riassunto è di circa due righe", body)
+    response = ask_model("Sei un assistente che riassume testi in italiano, generando riassunti brevi e coerenti, basati solo sul contenuto del testo, non aggiungere parole che non fanno riferimento al riassunto. Il riassunto è di circa due righe",
+                          body)
     return response
 
 @app.get("/notes/suggest_title/{note_id}",status_code=status.HTTP_200_OK)
 async def suggest_title(note_id: int):
     body = get_notebody_by_id(note_id)
-    response = ask_model('Sei un assistente che propone titoli brevi e coerenti per testi in italiano, basati solo sul contenuto del testo, non aggiungere parole che non fanno riferimento al titolo. non inserire alcun testo extra (tipo "Titolo:""), solo il titolo proposto. Non inserire caratteri speciali, tipo escape sequences', body)
+    response = ask_model('Sei un assistente che propone titoli brevi e coerenti per testi in italiano, basati solo sul contenuto del testo, non aggiungere parole che non fanno riferimento al titolo. non inserire alcun testo extra (tipo "Titolo:""), solo il titolo proposto. Non inserire caratteri speciali, tipo escape sequences',
+                          body)
     return response
